f6f582b4a9f0fd70c54ad605a58f2f24290df5c1
melchr <melchrstmn@gmail.com>
2020-07-17 18:07:57 -0400

    added event listener for backend

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const materialIndex = "localhost:3000/api/v1/materials"
+const materialIndex = "http://localhost:3000/api/v1/materials"
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    fetch(materialIndex)
+    .then(response => response.json())
+    .then(materials => {
+        console.log(materials)
+    })
 })
