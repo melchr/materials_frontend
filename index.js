d564ef3a28d1ea0d2a08a4e12cbd9ce3a484f3c0
melchr <melchrstmn@gmail.com>
2020-07-29 16:38:29 -0400

    added function for fetching materials

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const materialIndex = "http://localhost:3000/api/v1/materials"
 
 document.addEventListener('DOMContentLoaded', () => {
+    getMaterials()
+})
+
+function getMaterials() {
     fetch(materialIndex)
     .then(response => response.json())
     .then(materials => {
         console.log(materials)
     })
-})
+}
